889c931a18c27af4c47f1615987fb9b4be22b7e4
teackot <k.qovekt@gmail.com>
2022-02-05 19:30:41 +0300

    First commit. Rust code.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Valentines-Day
+
+Heart shaped code drawing a heart for Valentine's Day!
+
+## Contribution
+
+Feel free to add code in different programming languages
+
+## License
+
+Copyright (C) 2022 Nikita Kravets
+
+This App is distributed under terms of The GNU General Public License v3.0.
+
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate lazy_static;
+
+use colored::*;
+
+const NAME: &str = "Name"; // Change name here
+const NLEN: u32 = const_str::to_char_array!(NAME).len() as u32;
+lazy_static! { // Change colors here
+    static ref AT: String = "@".red().to_string();
+    static ref VNAME: String = NAME.green().to_string() + if NLEN % 2 == 0 {&AT} else {""};
+}
+const AT_LEN: u32 = (11 - NLEN) / 2;
+
+                                fn main       (){let
+                              charset:[&     str;3]=[" "
+                             ,&AT,&VNAME];  let heartmodl
+                            :Vec<Vec<(usize,u32) >>=vec![
+                             vec![(0,1),(1,5),(0,3),(1,5
+                              )],vec![(1,7), (00,1),(1,
+                               7)],vec![(0,1),(1,13)],
+                                vec![(0,2),(1,AT_LEN)
+                                 ,(2,1),(1,AT_LEN)],
+                                  vec![(0,3),(1,9)
+                                   ],vec![(0,4),(
+                                    1,7)],vec![(
+                                     0,6),(1,3)
+                                      ],] ;for
+                                        l in
+heartmodl{for sq in l{for _ in 0..sq.1{print!("{}", charset[sq.0]);}}println!();}}
