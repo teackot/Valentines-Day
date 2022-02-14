06659fae2baf82a27c6c6465f183493910625ec0
Victor B <victor.bonnelle@protonmail.com>
2022-02-14 20:04:07 +0100

    Add Python

---
+++ python/fmt.py
@@ -0,0 +1,31 @@
+from termcolor import colored as c
+
+n, l, h, w = c("Linux", "green"), len("Linux"), c("@", "red"), 15
+
+print(
+    "\n".join([
+        "\n".join([
+                  " " * (1 - i)
+                + h * (5 + i)
+                + " " * (3 - i)
+                + h * (5 + i) 
+            for i in [0,2]
+        ]),
+
+          " "
+        + h * (w - 2)
+        + " ",
+
+          " " * (4 // 2)
+        + h*(((w - 4) - l) // 2)
+        + c(n, "green")
+        + h*(((w - 4) - l) // 2),
+
+        "\n".join([
+                  " "*((w - i) // 2)
+                + h * i
+                + " " * ((w - i) // 2)
+            for i in range(9, 3, -2)
+        ])
+    ])
+)
